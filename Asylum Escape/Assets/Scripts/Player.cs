61903070121de06a6371d166f01a4bf874d48911
Milan <mujdar.milan@gmail.com>
2024-10-23 17:26:19 +0300

    Environment plus Basic Player and Camera Movement

--- Asylum Escape/Assets/Scripts/Player.cs
+++ Asylum Escape/Assets/Scripts/Player.cs
@@ -17,7 +17,7 @@ public class Player : MonoBehaviour
     private void Start()
     {
         _controller = gameObject.AddComponent<CharacterController>();
-        transform.position = new Vector3(0f, 0.76f, -6.0f); 
+        transform.position = new Vector3(0f, 0.76f, -6.0f);
     }
 
     void Update()
